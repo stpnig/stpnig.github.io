ed5a92e4a305bd445c27733aef88124ef35d1c48
stpnig <148638676+stpnig@users.noreply.github.com>
2024-09-21 17:50:02 +0300

    Add files via upload

---
+++ tapalka/1.js
@@ -0,0 +1,38 @@
+let pedro = document.querySelector(".p1")
+
+let btn1 = document.querySelector(".dtn-1")
+let btn2 = document.querySelector(".dtn-2")
+let dtn3 = document.querySelector(".dtn-3")
+let input = document.querySelector(".pole")
+let count = 0 
+let bied
+let ageinsec
+
+function writeText(){
+    pedro.innerHTML = input.value
+
+ 
+
+}
+function DeleteText(){
+    pedro.innerHTML = ""
+    count = 0
+    
+    }
+function tapayHamster(){
+    
+    bied= input.value + " " + count
+    pedro.innerHTML = bied
+    
+    count++
+        
+        
+        }
+function Ageinseconds(){
+    ageinsec= +input.value*365*24*60*60
+    pedro.innerHTML = ageinsec
+}        
+btn1.onclick = writeText
+btn2.onclick = DeleteText
+dtn3.onclick= tapayHamster
+dtn3.onclick= Ageinseconds
