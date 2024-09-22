706ac0c45c70d0bf540162b8cea28dac3ec9d9f2
stpnig <148638676+stpnig@users.noreply.github.com>
2024-09-22 09:27:27 +0300

    Update 1.js

--- tapalka/1.js
+++ tapalka/1.js
@@ -4,6 +4,7 @@ let btn1 = document.querySelector(".dtn-1")
 let btn2 = document.querySelector(".dtn-2")
 let dtn3 = document.querySelector(".dtn-3")
 let input = document.querySelector(".pole")
+let dtn4 = document.querySelector(".dtn-4")
 let count = 0 
 let bied
 let ageinsec
